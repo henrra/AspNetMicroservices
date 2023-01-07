1d30746cfb3367a2936807a2594bf520f6705bc7
RASATAMANANA <radohenintsoa.rasatamanana@gmail.com>
2023-01-07 23:29:41 +0100

    Miseà jour configuration Ocelot

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Prendre en compte les fichiers de configuration ocelot.{env}.json
 builder.Configuration.AddOcelot(builder.Environment);
+var envName = builder.Environment.IsProduction() ? string.Empty : $".{builder.Environment.EnvironmentName}";
+builder.Configuration.AddJsonFile($"ocelot{envName}.json", optional: false, reloadOnChange: false);
 
 // Ajout services
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 app.UseOcelot().Wait();
-app.MapGet("/", () => "Hello World!");
 app.Run();
